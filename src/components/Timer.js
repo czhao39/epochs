5da23cbb9a81cc9bb172b84f42f170b818accb2b
Haicheng Charles Zhao <czhao39@gmail.com>
2017-08-03 10:19:37 -0400

    add pause overlay to timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,18 +5,20 @@ import "../styles/Timer.scss";
 import { secsToDuration, durationToString } from "../helpers";
 
 
-const Timer = ({ secsRemaining, togglePaused }) => {
+const Timer = ({ paused, secsRemaining, togglePaused }) => {
     return (
-        <div 
-            className="timer"
+        <div
+            className={`timer${paused ? " paused" : ""}`}
             onClick={() => togglePaused()}
         >
             {durationToString(secsToDuration(secsRemaining))}
+            <div className={`pause-overlay${paused ? " paused" : ""}`}><div><i className="fa fa-fw fa-pause" /></div></div>
         </div>
     );
 };
 
 Timer.propTypes = {
+    paused: PropTypes.bool.isRequired,
     secsRemaining: PropTypes.number.isRequired,
     togglePaused: PropTypes.func.isRequired,
 };
--- src/containers/CurTaskBox.js
+++ src/containers/CurTaskBox.js
@@ -17,6 +17,7 @@ export default class CurTaskBox extends Component {
         return (
             <div>
                 <Timer
+                    paused={this.props.tasks.paused}
                     secsRemaining={this.props.tasks.list.length > 0 ? this.props.tasks.list[0].secsRemaining : 0}
                     togglePaused={this.props.togglePaused}
                 />
