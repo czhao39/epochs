9b27d24ffe4b0e09333f87836f3cb320fc416904
Haicheng Charles Zhao <czhao39@gmail.com>
2017-08-04 04:24:40 -0400

    fix error on all tasks finished

--- src/containers/CurTaskBox.js
+++ src/containers/CurTaskBox.js
@@ -23,23 +23,30 @@ export default class CurTaskBox extends Component {
                         togglePaused={this.props.togglePaused}
                     />
                 </div>
-                <div className="cur-task">
-                    {this.props.tasks.list[0].name}
-                </div>
-                <div className="cur-task-controls">
-                    <i
-                        className="done-button fa fa-fw fa-check"
-                        onClick={() => this.props.finishTask(0, true, this.props.tasks.list[0].name)}
-                    />
-                    <i
-                        className="cancel-button fa fa-fw fa-close"
-                        onClick={() => this.props.finishTask(0, false)}
-                    />
-                    <i
-                        className="edit-button fa fa-fw fa-pencil"
-                        onClick={() => this.props.toggleEditTaskModal(true, 0)}
-                    />
-                </div>
+                {
+                    this.props.tasks.list.length > 0 ?
+                        <div>
+                            <div className="cur-task">
+                                {this.props.tasks.list[0].name}
+                            </div>
+                            <div className="cur-task-controls">
+                                <i
+                                    className="done-button fa fa-fw fa-check"
+                                    onClick={() => this.props.finishTask(0, true, this.props.tasks.list[0].name)}
+                                />
+                                <i
+                                    className="cancel-button fa fa-fw fa-close"
+                                    onClick={() => this.props.finishTask(0, false)}
+                                />
+                                <i
+                                    className="edit-button fa fa-fw fa-pencil"
+                                    onClick={() => this.props.toggleEditTaskModal(true, 0)}
+                                />
+                            </div>
+                        </div>
+                    :
+                        <div>You're done!</div>
+                }
             </div>
         );
     }
