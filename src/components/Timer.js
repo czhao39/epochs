0533ddbfec2380ca4d8fea814ce07888ebd3bb27
Haicheng Charles Zhao <czhao39@gmail.com>
2017-08-09 03:00:57 -0400

    create initial timer animation

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
+import { Motion, spring } from "react-motion";
 
 import "../assets/css/Timer.scss";
 import { secsToDuration, durationToString } from "../helpers";
 
 
-const Timer = ({ paused, secsRemaining, color, togglePaused }) => {
-    return (
-        <div
-            className={`${color} timer${paused ? " paused" : ""}`}
-            onClick={() => togglePaused()}
-        >
-            {durationToString(secsToDuration(secsRemaining))}
-            <div className={`pause-overlay${paused ? " paused" : ""}`}><i className="fa fa-fw fa-pause" /></div>
-        </div>
-    );
-};
+class Timer extends PureComponent {
+    render() {
+        return (
+            <div
+                className="timer"
+                onClick={() => this.props.togglePaused()}
+            >
+                <Motion
+                    defaultStyle={{ theta: 0 }}
+                    style={{ theta: spring(302, { stiffness: 10, damping: 6 }) }}
+                >
+                    {(interpolatingStyle) => (
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 100 100"
+                            className={`timer-circle ${this.props.paused ? " paused" : ""}`}
+                        >
+                            <circle
+                                ref={(ref) => this.circle = ref}
+                                className={this.props.color}
+                                cx="50" cy="50" r="48"
+                                fill="none"
+                                strokeWidth="4"
+                                strokeDasharray="302"
+                                strokeDashoffset={302 - interpolatingStyle.theta}
+                                transform="rotate(-90, 50, 50)"/>
+                            />
+                            <text
+                                textAnchor="middle"
+                                x="50" y="57"
+                            >
+                                {durationToString(secsToDuration(this.props.secsRemaining))}
+                            </text>
+                        </svg>
+                    )}
+                </Motion>
+                <div className={`pause-overlay${this.props.paused ? " paused" : ""}`}><i className="fa fa-fw fa-pause" /></div>
+            </div>
+        );
+    }
+}
 
 Timer.propTypes = {
     paused: PropTypes.bool.isRequired,
