5dd7fa32342e4f1a8017770d754f3af0f333d69a
Haicheng Charles Zhao <czhao39@gmail.com>
2017-08-10 08:08:55 -0400

    make theme color darker blue grey

--- src/reducers/tasksReducer.js
+++ src/reducers/tasksReducer.js
@@ -10,8 +10,8 @@ import { durationToSecs } from "../helpers";
 const DEFAULT_TASKS = {
     paused: true,
     list: [
-        { name: "I'm an epoch!", secsRemaining: 10, color: "blue" },
-        { name: "I'm another epoch!", secsRemaining: 30, color: "grey" },
+        { name: "I'm an epoch!", secsRemaining: 10, color: "grey" },
+        { name: "I'm another epoch!", secsRemaining: 30, color: "green" },
     ],
 }
 
